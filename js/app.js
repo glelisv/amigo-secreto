5c50eacbdf1b7fb3867651a2112fd5c81389dc69
Gustavo Lelis <gustavolelis_bh@hotmail.com>
2024-10-15 20:47:58 -0300

    botao adicionar implementado

---
+++ js/app.js
@@ -0,0 +1,18 @@
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');
+
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+
+    amigo.value = '';
+}
+function sortear() {
+
+}
+function reiniciar() {
+
+}
