400c2266054ba913ec737345ef5f292dbced5e52
Gustavo Lelis <gustavolelis_bh@hotmail.com>
2024-10-15 21:17:09 -0300

    funcao sortear implementados

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
+let amigos = [];
+
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
+    amigos.push(amigo.value);
 
     if (lista.textContent == '') {
         lista.textContent = amigo.value;
@@ -11,8 +14,26 @@ function adicionar() {
     amigo.value = '';
 }
 function sortear() {
-
+    embaralhar(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[0] + '<br/>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[i + 1] + '<br/>';
+        }
+    }
 }
-function reiniciar() {
 
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
 }
